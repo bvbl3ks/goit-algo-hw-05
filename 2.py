552c75e9d2578860a3f482053e4ab6ce7a48696f
bvbl3ks <113239848+bvbl3ks@users.noreply.github.com>
2026-01-18 19:21:52 +0100

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+import re
+from typing import Callable
+
+def generator_numbers(text: str):
+    numbers = re.findall(r"\b\d+\.?\d*\b", text)
+    for num in numbers:
+        yield float(num)
+
+def sum_profit(text: str, func: Callable):
+    total = sum(func(text))
+    return round(total, 2)
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
